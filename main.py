7204fb6dab1a3fd51591cfc2655018986ea80703
Leslie Lee <leslie@doubleldesign.com>
2017-09-14 20:57:15 -0500

    flicklist1 almost finished, but not quite

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import random
 
 app = Flask(__name__)
 
@@ -14,16 +15,28 @@ def index():
     content += "<ul>"
     content += "<li>" + movie + "</li>"
     content += "</ul>"
+    
+    return content
 
+def index():
+    # choose a movie by invoking our new function
+    movie = get_random_movie()
     # TODO: pick another random movie, and display it under
     # the heading "<h1>Tommorrow's Movie</h1>"
+    content = "<h1>Tomorrow's Movie</h1>"
+    content += "<ul>"
+    content += "<li>" + movie + "</li>"
+    content += "</ul>"
 
     return content
 
 def get_random_movie():
     # TODO: make a list with at least 5 movie titles
+    movies = ["Pirates of the Caribbean", "Hairspray", "Aladdin", "Forrest Gump", "Finding Nemo"]
     # TODO: randomly choose one of the movies, and return it
-    return "The Big Lebowski"
+    rand_movie = random.randint(1, len(movies))
+    rand_movie = movies[rand_movie]
+    return rand_movie
 
 
 app.run()
